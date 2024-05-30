24045245c1a3f11ce4b0b82695e33586389412ca
Paras Khonde <pkhonde@weltman.com>
2024-05-30 14:46:16 +0530

    dotnet project added with test units

---
+++ WebApp/webTest/webTest.cs
@@ -0,0 +1,26 @@
+using WebApp.Modules;
+
+namespace webTest
+{
+    public class webTest
+    {
+        [Fact]
+        public void DemoTest()
+        {
+            int i = 1;
+            bool result = false;
+            if (i == 1) result = true;
+            Assert.True(result, "Value should be equal to 1");
+        }
+
+        [Fact]
+        public void CheckAddFunction()
+        {
+            Functions fun = new Functions();
+            int y = fun.Add(3, 2);
+            bool result = false;
+            if (y == 5) result = true;
+            Assert.True(result, "Value should be equal to 5");
+        }
+    }
+}
