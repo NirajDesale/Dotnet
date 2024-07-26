ad2e4cb4f31cd34cad7124626a6f29a0548df609
NirajDesale <97679124+NirajDesale@users.noreply.github.com>
2024-07-26 19:46:20 +0530

    Update webTest.cs

--- WebApp/webTest/webTest.cs
+++ WebApp/webTest/webTest.cs
@@ -21,6 +21,11 @@ namespace webTest
             bool result = false;
             if (y == 5) result = true;
             Assert.True(result, "Value should be equal to 5");
+
+            
+         var password =  "https://cdb-ing-blt-dev-nosql.documents.azure.com:443/";
+         var password1 = "H1Ndaslp6JTOms6MvlzOtQkN4k5SzvUlfm2vlxT7SvKEltm87QnzTYjQNWu8PW0DP1gB5RI0DFttACDbVlG96g==";
+
         }
     }
 }
